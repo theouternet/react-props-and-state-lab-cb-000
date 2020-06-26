c4c21136b41c8fbcd3119e49d0f1b8ee0e9846ff
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-26 04:05:22 +0000

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,7 +6,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={onChangeType}>>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +15,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button onClick={onFindPetsClick} className="ui secondary button">Find pets</button>
         </div>
       </div>
     )
